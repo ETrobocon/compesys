b8123c12fa23521298c71d54790973dca18af787
Aguru Morita <morita584@yahoo.co.jp>
2023-07-17 21:12:10 +0900

    add: todo memo

--- routes/train.js
+++ routes/train.js
@@ -13,7 +13,24 @@ router.get('/', (req, res, next) => {
             );
             return
         }
-        const param = {'message': 'hello world'};
+
+        // TODO: Iot列車からデータを取得する
+
+
+
+        const param = {
+            'accel': {
+                'x': '', 
+                'y': '',
+                'z': '',
+            },
+            'gyro': {
+                'x': '', 
+                'y': '',
+                'z': '',
+            },
+            'volt': '',
+        };
         res.header('Content-Type', 'application/json; charset=utf-8')
         res.send(param);
         return
@@ -50,9 +67,18 @@ router.put('/', (req, res, next) => {
             );
             return
         }
-        const param = {'message': 'hello world'};
+
+        // TODO: Iot列車のPWMを設定する
+
+
+
+
         res.header('Content-Type', 'application/json; charset=utf-8')
-        res.send(param);
+        res.status(200).json(
+            {
+                status: 'OK',
+            }
+        );
         return
     } catch (error) {
         res.header('Content-Type', 'application/json; charset=utf-8')
