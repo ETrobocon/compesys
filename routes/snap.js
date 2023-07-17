adab0bf85745e4d16a4d38574e47ee1569d45b70
Aguru Morita <morita584@yahoo.co.jp>
2023-07-17 12:37:13 +0900

    changed: Store image files included in the request body locally

--- routes/snap.js
+++ routes/snap.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const fs = require("fs");
 const router = express.Router();
 
-router.post('/snap', (req, res, next) => {
-    const param = {'message': 'hello world'};
+router.post('/', bodyParser.raw({type: ["image/png"]}), (req, res, next) => {
+    fs.writeFile('image.jpeg', req.body, (error) => {
+        if (error) {
+            throw error;
+        }
+    });
     res.header('Content-Type', 'application/json; charset=utf-8')
-    res.send(param);
+    res.status(201).json({status: 'Created'});
+    return
 });
 
 module.exports = router;
