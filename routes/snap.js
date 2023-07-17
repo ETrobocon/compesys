808049ed9e814dccf5065bfe6fbe137b9ec5e785
Aguru Morita <morita584@yahoo.co.jp>
2023-07-17 17:23:33 +0900

    change: do not accept requests if status is not in competition

--- routes/snap.js
+++ routes/snap.js
@@ -2,10 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const fs = require("fs");
 const router = express.Router();
+const STATE = require('./constants').STATE;
 
 router.post('/', bodyParser.raw({type: ["image/png"]}), (req, res, next) => {
     try {
-        console.log(req.get('Content-Type'));
+        if (app.get('state') !== STATE.IN_COMPETITION) {
+            res.status(403).json(
+                {
+                    status: 'Forbidden',
+                    message: 'Request not currently allowed',
+                }
+            );
+            return
+        }
+        if (req.get('Content-Type') === 'image/png') {
+            res.header('Content-Type', 'application/json; charset=utf-8')
+            res.status(400).json(
+                {
+                    status: 'Bad Request',
+                    message: 'Unexpected content type',
+                }
+            );
+            return
+        }
         if (req.get('Content-Type') === 'image/png') {
             res.header('Content-Type', 'application/json; charset=utf-8')
             res.status(400).json(
--- routes/train.js
+++ routes/train.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const STATE = require('./constants').STATE;
 
 router.get('/', (req, res, next) => {
     try {
+        if (app.get('state') !== STATE.IN_COMPETITION) {
+            res.status(403).json(
+                {
+                    status: 'Forbidden',
+                    message: 'Request not currently allowed',
+                }
+            );
+            return
+        }
         const param = {'message': 'hello world'};
         res.header('Content-Type', 'application/json; charset=utf-8')
         res.send(param);
@@ -20,6 +30,15 @@ router.get('/', (req, res, next) => {
 
 router.put('/', (req, res, next) => {
     try {
+        if (app.get('state') !== STATE.IN_COMPETITION) {
+            res.status(403).json(
+                {
+                    status: 'Forbidden',
+                    message: 'Request not currently allowed',
+                }
+            );
+            return
+        }
         const pwm = Number(req.query.pwm)
         if (pwm === NaN || !(pwm >= 0 && pwm <= 100)) {
             res.header('Content-Type', 'application/json; charset=utf-8')
