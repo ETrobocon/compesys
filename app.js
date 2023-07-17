1ebe986cca3f61cd914787ce45cbebdba30ad01a
Aguru Morita <morita584@yahoo.co.jp>
2023-07-17 10:03:41 +0900

    change: attach hello

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
 
-const train = require('./routes/train');
+const hello = require('./routes/hello');
 const snap = require('./routes/snap');
+const train = require('./routes/train');
 
-app.use('/train', train);
+app.use('/', hello);  
 app.use('/snap', snap);   
+app.use('/train', train);
 
 const server = app.listen(8080, () => {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
-
-app.get('/', (req, res) => {
-    res.json({ 'message': 'hello world' })
-})
