8f376177f59f353229753c6d440842d3f34bc029
Aguru Morita <morita584@yahoo.co.jp>
2023-07-17 10:00:27 +0900

    change: attach train and snap

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+const train = require('./routes/train');
+const snap = require('./routes/snap');
+
+app.use('/train', train);
+app.use('/snap', snap);   
 
 const server = app.listen(8080, () => {
     console.log("Node.js is listening to PORT:" + server.address().port);
