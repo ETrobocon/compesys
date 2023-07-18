26a8f0786ce9912d01a9e77f2d6400b7bb7082f0
Aguru Morita <morita584@yahoo.co.jp>
2023-07-19 08:27:06 +0900

    change: Definition of constant STATE

--- constants.js
+++ constants.js
@@ -1,6 +1,9 @@
 const STATE = {
-    STAND_BY : 0,
-    IN_COMPETITION : 1,
+    UNDEFINDED: 0,  // 未定義
+    READY: 1,       // スタート前
+    RUNNING: 2,     // 走行中
+    PASSING: 3,     // LAP通過
+    GOAL: 4,        // ゴール
 }
 
 module.exports = {
