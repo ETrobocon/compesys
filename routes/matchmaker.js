93d5f3a2b29713c48eef324a8d2152ac81edcdbd
Aguru Morita <morita584@yahoo.co.jp>
2023-07-20 00:39:23 +0900

    add:api to get image

--- routes/matchmaker.js
+++ routes/matchmaker.js
@@ -1,3 +1,5 @@
+const archiver = require('archiver');
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 const { STATE, TEMP_DIR } = require('../constants');
@@ -32,7 +34,38 @@ router.put('/state/:trigger', (req, res, next) => {
         return 
     }
 });
-        return
+
+router.get('/image/:id', (req, res, next) => {
+    try {
+        const id = req.params.id
+
+        // 出力先のzipファイル名
+        const zipPath = `${TEMP_DIR}/${id}.zip`;
+        const targetDirectory = `${TEMP_DIR}/${id}`;
+
+        // ストリームを生成して、archiverと紐付ける
+        const archive = archiver.create('zip', {
+            zlib: { level: 9 } // Sets the compression level.
+        });
+        const output = fs.createWriteStream(zipPath);
+        archive.pipe(output);
+
+        // 圧縮対象のファイル及びフォルダ
+        archive.directory(targetDirectory, false);
+
+
+        // zip圧縮実行
+        archive.finalize();
+        output.on("close", () => {
+            // zip圧縮完了すると発火する
+            // const archive_size = archive.pointer();
+            // console.log(`complete! total size : ${archive_size} bytes`);
+            
+            res.header('Content-Type', 'application/zip;')
+            res.header('Content-Disposition', 'attachment;')
+            res.status(200).sendFile(zipPath);           
+            return
+        });
     } catch (error) {
         res.header('Content-Type', 'application/json; charset=utf-8')
         res.status(500).json(
