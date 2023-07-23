3dfb8374b1099f600b5f5984cfd6eac3ca17dd62
Aguru Morita <morita584@yahoo.co.jp>
2023-07-23 22:19:52 +0900

    Delete unnecessary comments

--- routes/matchmaker.js
+++ routes/matchmaker.js
@@ -47,28 +47,19 @@ router.get('/image/:id', (req, res, next) => {
     try {
         const id = req.params.id
 
-        // 出力先のzipファイル名
         const zipPath = `${TEMP_DIR}/${id}.zip`;
         const targetDirectory = `${TEMP_DIR}/${id}`;
 
-        // ストリームを生成して、archiverと紐付ける
         const archive = archiver.create('zip', {
             zlib: { level: 9 } // Sets the compression level.
         });
         const output = fs.createWriteStream(zipPath);
         archive.pipe(output);
 
-        // 圧縮対象のファイル及びフォルダ
         archive.directory(targetDirectory, false);
 
-
-        // zip圧縮実行
         archive.finalize();
         output.on("close", () => {
-            // zip圧縮完了すると発火する
-            // const archive_size = archive.pointer();
-            // console.log(`complete! total size : ${archive_size} bytes`);
-            
             res.header('Content-Type', 'application/zip;')
             res.header('Content-Disposition', 'attachment;')
             res.status(200).sendFile(zipPath);           
