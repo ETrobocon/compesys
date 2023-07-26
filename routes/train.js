05fc325c5b496071724e646610f0efaa6a061343
Aguru Morita <morita584@yahoo.co.jp>
2023-07-26 22:12:06 +0900

    added code to get sensor value from xiao

--- routes/train.js
+++ routes/train.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { STATE } = require('../constants');
 const iottrain = require('../iottrain_central');
 
-router.get('/', (req, res, next) => {
+router.get('/', async(req, res, next) => {
     try {
         if ( req.app.get('state') === STATE.READY ||
             req.app.get('state') === STATE.GOAL) {
@@ -16,13 +16,18 @@ router.get('/', (req, res, next) => {
             );
             return;
         }   
+ 
+        await Promise.all([
+            getAccelerometer(),
+            getGyroscope(),
+            getVoltage(),
+        ]);
 
-        // TODO: Iot列車からデータを取得する
         const accel = iottrain.inbox.accelerometer;
         const gyro = iottrain.inbox.gyroscope;
         const temp = iottrain.inbox.temperature;
         const volt = iottrain.inbox.voltage;
-
+        
         console.log("accel@" + accel.timestamp + ": " + accel.x + ", " + accel.y + ", " + accel.z);
         console.log(" gyro@" + gyro.timestamp + ": " + gyro.x + ", " + gyro.y + ", " + gyro.z);
         console.log(" temp@" + temp.timestamp + ": " + temp.value + "℃");
@@ -30,16 +35,16 @@ router.get('/', (req, res, next) => {
 
         const param = {
             'accel': {
-                'x': accel.x.toString(), 
-                'y': accel.y.toString(),
-                'z': accel.z.toString(),
+                'x': accel.x, 
+                'y': accel.y, 
+                'z': accel.z, 
             },
             'gyro': {
-                'x': gyro.x.toString(), 
-                'y': gyro.y.toString(),
-                'z': gyro.z.toString(),
+                'x': gyro.x, 
+                'y': gyro.y, 
+                'z': gyro.z, 
             },
-            'volt': volt.value.toString(),
+            'volt': volt.value, 
         };
         res.header('Content-Type', 'application/json; charset=utf-8')
         res.send(param);
@@ -78,10 +83,6 @@ router.put('/', (req, res, next) => {
             return;
         }
 
-        const date = new Date();
-        iottrain.inbox.pwm.timestamp = date.getTime();
-        iottrain.inbox.pwm.value = pwm;
-
         res.header('Content-Type', 'application/json; charset=utf-8')
         res.status(200).json(
             {
@@ -90,6 +91,7 @@ router.put('/', (req, res, next) => {
         );
         return;
     } catch (error) {
+        console.log(error)
         res.header('Content-Type', 'application/json; charset=utf-8')
         res.status(500).json(
             {
@@ -100,4 +102,47 @@ router.put('/', (req, res, next) => {
     }
 });
 
+const getAccelerometer = () => {
+    return new Promise((resolve, reject) => {
+        iottrain.characteristics["accelerometer"].instance.read((error, data) => {
+            return resolve(data)
+        })
+    }).then(data => {
+        iottrain.inbox["accelerometer"].x = data.readFloatLE(4);
+        iottrain.inbox["accelerometer"].y = data.readFloatLE(8);
+        iottrain.inbox["accelerometer"].z = data.readFloatLE(12);
+        return;
+    }).catch(error => {
+        console.log(error);
+    })
+  }
+  
+const getGyroscope = () => {
+    return new Promise((resolve, reject) => {
+        iottrain.characteristics["gyroscope"].instance.read((error, data) => {
+            return resolve(data)
+        });
+    }).then(data => {
+        iottrain.inbox["gyroscope"].x = data.readFloatLE(4);
+        iottrain.inbox["gyroscope"].y = data.readFloatLE(8);
+        iottrain.inbox["gyroscope"].z = data.readFloatLE(12);
+        return;
+    }).catch(error => {
+        console.log(error);
+    })
+}
+  
+const getVoltage = () => {
+    return new Promise((resolve, reject) => {
+        iottrain.characteristics["voltage"].instance.read((error, data) => {
+            return resolve(data)
+        });
+    }).then(data =>{
+        iottrain.inbox["voltage"].value = data.readFloatLE(4);
+        return;
+    }).catch(error => {
+        console.log(error);
+    })
+}
+  
 module.exports = router;
