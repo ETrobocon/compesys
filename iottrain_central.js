8b18646cd9cf84f1cadf72f8c9892f2d2e7f3fc4
Aguru Morita <morita584@yahoo.co.jp>
2023-07-30 22:08:46 +0900

    Apply formatter

--- iottrain_central.js
+++ iottrain_central.js
@@ -1,188 +1,196 @@
-const noble = require('@abandonware/noble');
-const { logger } = require('./logger.js');
-const loggerChild = logger.child({ domain: 'iottrain_central' })
+const noble = require("@abandonware/noble");
+const { logger } = require("./logger.js");
+const loggerChild = logger.child({ domain: "iottrain_central" });
 
-const sleep = (msec) => new Promise(resolve => setTimeout(resolve, msec));
+const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
 
 // BLE peripheral GATT profile: XIAO side
 const GATT_PROFILE = {
-  'services': {
-    'xiao': {
-      'uuid': 'AD0C1000-64E9-48B0-9088-6F9E9FE4972E',
-      'characteristics': {
-        'command': {
-          'uuid': 'AD0C1001-64E9-48B0-9088-6F9E9FE4972E',
-          'isReadable': false,
-          'isWritable': true,
-          'isNotifiable': true,
-          'packetLength': 21
+  services: {
+    xiao: {
+      uuid: "AD0C1000-64E9-48B0-9088-6F9E9FE4972E",
+      characteristics: {
+        command: {
+          uuid: "AD0C1001-64E9-48B0-9088-6F9E9FE4972E",
+          isReadable: false,
+          isWritable: true,
+          isNotifiable: true,
+          packetLength: 21,
         },
-        'accelerometer': {
-          'uuid': 'AD0C1002-64E9-48B0-9088-6F9E9FE4972E',
-          'isReadable': true,
-          'isWritable': false,
-          'isNotifiable': true,
-          'packetLength': 16
+        accelerometer: {
+          uuid: "AD0C1002-64E9-48B0-9088-6F9E9FE4972E",
+          isReadable: true,
+          isWritable: false,
+          isNotifiable: true,
+          packetLength: 16,
         },
-        'gyroscope': {
-          'uuid': 'AD0C1003-64E9-48B0-9088-6F9E9FE4972E',
-          'isReadable': true,
-          'isWritable': false,
-          'isNotifiable': true,
-          'packetLength': 16
+        gyroscope: {
+          uuid: "AD0C1003-64E9-48B0-9088-6F9E9FE4972E",
+          isReadable: true,
+          isWritable: false,
+          isNotifiable: true,
+          packetLength: 16,
         },
-        'temperature': {
-          'uuid': 'AD0C1004-64E9-48B0-9088-6F9E9FE4972E',
-          'isReadable': true,
-          'isWritable': false,
-          'isNotifiable': true,
-          'packetLength': 4
+        temperature: {
+          uuid: "AD0C1004-64E9-48B0-9088-6F9E9FE4972E",
+          isReadable: true,
+          isWritable: false,
+          isNotifiable: true,
+          packetLength: 4,
         },
-        'led': {
-          'uuid': 'AD0C1005-64E9-48B0-9088-6F9E9FE4972E',
-          'isReadable': true,
-          'isWritable': true,
-          'isNotifiable': false,
-          'packetLength': 1
+        led: {
+          uuid: "AD0C1005-64E9-48B0-9088-6F9E9FE4972E",
+          isReadable: true,
+          isWritable: true,
+          isNotifiable: false,
+          packetLength: 1,
         },
-        'pwm': {
-          'uuid': 'AD0C2001-64E9-48B0-9088-6F9E9FE4972E',
-          'isReadable': true,
-          'isWritable': true,
-          'isNotifiable': false,
-          'packetLength': 1
+        pwm: {
+          uuid: "AD0C2001-64E9-48B0-9088-6F9E9FE4972E",
+          isReadable: true,
+          isWritable: true,
+          isNotifiable: false,
+          packetLength: 1,
         },
-        'voltage': {
-          'uuid': 'AD0C2002-64E9-48B0-9088-6F9E9FE4972E',
-          'isReadable': true,
-          'isWritable': false,
-          'isNotifiable': true,
-          'packetLength': 8
-        }
-      }
-    }
-  }
-}
+        voltage: {
+          uuid: "AD0C2002-64E9-48B0-9088-6F9E9FE4972E",
+          isReadable: true,
+          isWritable: false,
+          isNotifiable: true,
+          packetLength: 8,
+        },
+      },
+    },
+  },
+};
 
 noble.characteristics = GATT_PROFILE.services.xiao.characteristics;
 noble.servicesDiscovered = false;
 noble.inbox = {
-  'accelerometer': {
-    'timestamp': 0,
-    'x': 0,
-    'y': 0,
-    'z': 0
+  accelerometer: {
+    timestamp: 0,
+    x: 0,
+    y: 0,
+    z: 0,
   },
-  'gyroscope': {
-    'timestamp': 0,
-    'x': 0,
-    'y': 0,
-    'z': 0
+  gyroscope: {
+    timestamp: 0,
+    x: 0,
+    y: 0,
+    z: 0,
   },
-  'temperature': {
-    'timestamp': 0,
-    'value': 0
+  temperature: {
+    timestamp: 0,
+    value: 0,
   },
-  'voltage': {
-    'timestamp': 0,
-    'value': 0
+  voltage: {
+    timestamp: 0,
+    value: 0,
   },
-  'pwm': {
-    'timestamp': 0,
-    'value': 0
-  }
-}
+  pwm: {
+    timestamp: 0,
+    value: 0,
+  },
+};
 noble.connected = false;
 
-noble.on('stateChange', async (state) => {
-  loggerChild.info('[noble]onStateChange: ' + state);
-  if (state === 'poweredOn') {
+noble.on("stateChange", async (state) => {
+  loggerChild.info("[noble]onStateChange: " + state);
+  if (state === "poweredOn") {
     await noble.startScanningAsync();
   }
 });
 
-noble.on('scanStart', () => {
-  loggerChild.info('[noble]Start scanning');
+noble.on("scanStart", () => {
+  loggerChild.info("[noble]Start scanning");
 });
 
-noble.on('scanStop', () => {
-  loggerChild.info('[noble]Stop scanning');
+noble.on("scanStop", () => {
+  loggerChild.info("[noble]Stop scanning");
 });
 
-noble.on('discover', async (peripheral) => {
+noble.on("discover", async (peripheral) => {
   let localName = peripheral.advertisement.localName;
-  if (localName && 
-    (
-      (process.env.MY_XIAO !== '' && process.env.MY_XIAO === localName) 
-      || 
-      (process.env.MY_XIAO === '' && localName.startsWith("XIAO"))
-    )) {        
-    loggerChild.info('[noble]discovered: ' + localName);
+  if (
+    localName &&
+    ((process.env.MY_XIAO !== "" && process.env.MY_XIAO === localName) ||
+      (process.env.MY_XIAO === "" && localName.startsWith("XIAO")))
+  ) {
+    loggerChild.info("[noble]discovered: " + localName);
     await noble.stopScanningAsync();
     noble.servicesDiscovered = false;
     peripheral.connect();
 
-    peripheral.once('connect', async function() {
+    peripheral.once("connect", async function () {
       noble.connected = true;
-      loggerChild.info('[noble]connected.');
+      loggerChild.info("[noble]connected.");
       await this.discoverServicesAsync();
       await waitForDiscover();
       for (const key in noble.characteristics) {
         const characteristic = noble.characteristics[key];
         const instance = characteristic.instance;
         if (characteristic.isNotifiable) {
-          loggerChild.info('[noble]subscribe: ' + key);
+          loggerChild.info("[noble]subscribe: " + key);
           instance.subscribeAsync();
           //instance.on('data', async (data, isNotification) => {
-            //console.log(data);
-            //console.log(isNotification);
-            // let dv = new DataView(data.buffer);
-            // noble.inbox[key].timestamp = dv.getUint32(0, true);
-            // switch (key) {
-            //   case 'accelerometer':
-            //   case 'gyroscope':
-            //     noble.inbox[key].x = dv.getFloat32(4, true);
-            //     noble.inbox[key].y = dv.getFloat32(8, true);
-            //     noble.inbox[key].z = dv.getFloat32(12, true);
-            //     break;
+          //console.log(data);
+          //console.log(isNotification);
+          // let dv = new DataView(data.buffer);
+          // noble.inbox[key].timestamp = dv.getUint32(0, true);
+          // switch (key) {
+          //   case 'accelerometer':
+          //   case 'gyroscope':
+          //     noble.inbox[key].x = dv.getFloat32(4, true);
+          //     noble.inbox[key].y = dv.getFloat32(8, true);
+          //     noble.inbox[key].z = dv.getFloat32(12, true);
+          //     break;
 
-            //   case 'temperature':
-            //   case 'voltage':
-            //     noble.inbox[key].value = dv.getFloat32(4, true);
-            //     break;
-            // }
+          //   case 'temperature':
+          //   case 'voltage':
+          //     noble.inbox[key].value = dv.getFloat32(4, true);
+          //     break;
+          // }
           //});
         }
       }
       loop();
     });
 
-    peripheral.once('disconnect', async () => {
+    peripheral.once("disconnect", async () => {
       noble.connected = false;
-      loggerChild.info('[noble]disconnected.');
+      loggerChild.info("[noble]disconnected.");
       await noble.startScanningAsync();
     });
 
-    peripheral.once('servicesDiscover', async (services) => {
-      loggerChild.info('[noble]discovering services');
+    peripheral.once("servicesDiscover", async (services) => {
+      loggerChild.info("[noble]discovering services");
       for (i = 0; i < services.length; i++) {
-        if (services[i].uuid.toUpperCase() === GATT_PROFILE.services.xiao.uuid.replace(/-/g, "")) {
-          services[i].once('includedServicesDiscover', async function() {
-            loggerChild.info('[noble]discovering includedServices');
+        if (
+          services[i].uuid.toUpperCase() ===
+          GATT_PROFILE.services.xiao.uuid.replace(/-/g, "")
+        ) {
+          services[i].once("includedServicesDiscover", async function () {
+            loggerChild.info("[noble]discovering includedServices");
             await this.discoverCharacteristicsAsync();
           });
 
-          services[i].once('characteristicsDiscover', async (characteristics) => {
-            loggerChild.info('[noble]discovering characteristics');
-            for (j = 0; j < characteristics.length; j++) {
-              for (const key in noble.characteristics) {
-                if (characteristics[j].uuid.toUpperCase() === noble.characteristics[key].uuid.replace(/-/g, "")) {
-                  noble.characteristics[key].instance = characteristics[j];
+          services[i].once(
+            "characteristicsDiscover",
+            async (characteristics) => {
+              loggerChild.info("[noble]discovering characteristics");
+              for (j = 0; j < characteristics.length; j++) {
+                for (const key in noble.characteristics) {
+                  if (
+                    characteristics[j].uuid.toUpperCase() ===
+                    noble.characteristics[key].uuid.replace(/-/g, "")
+                  ) {
+                    noble.characteristics[key].instance = characteristics[j];
+                  }
                 }
               }
+              noble.servicesDiscovered = true;
             }
-            noble.servicesDiscovered = true;
-          });
+          );
 
           await services[i].discoverIncludedServicesAsync();
         }
@@ -197,83 +205,90 @@ async function waitForDiscover() {
   }
 }
 
-const loop = async() => {
+const loop = async () => {
   while (true) {
     if (!noble.connected) {
       break;
     }
-    await Promise.all([
-      getAccelerometer(),
-      getGyroscope(),
-      getVoltage(),
-    ]);
+    await Promise.all([getAccelerometer(), getGyroscope(), getVoltage()]);
     await sleep(100);
   }
-}
+};
 
 const getAccelerometer = () => {
   return new Promise((resolve, reject) => {
-      noble.characteristics["accelerometer"].instance.read((error, data) => {
-          if (error !== null) {
-              return reject(error);
-          }    
-          return resolve(data);
-      })
-  }).then(data => {
+    noble.characteristics["accelerometer"].instance.read((error, data) => {
+      if (error !== null) {
+        return reject(error);
+      }
+      return resolve(data);
+    });
+  })
+    .then((data) => {
       noble.inbox["accelerometer"].x = data.readFloatLE(4);
       noble.inbox["accelerometer"].y = data.readFloatLE(8);
       noble.inbox["accelerometer"].z = data.readFloatLE(12);
       return;
-  }).catch(error => {
+    })
+    .catch((error) => {
       loggerChild.error(error);
       noble.inbox["accelerometer"].x = null;
       noble.inbox["accelerometer"].y = null;
       noble.inbox["accelerometer"].z = null;
       return;
-  })
-}
+    });
+};
 
 const getGyroscope = () => {
   return new Promise((resolve, reject) => {
     noble.characteristics["gyroscope"].instance.read((error, data) => {
       if (error !== null) {
-          return reject(error);
+        return reject(error);
       }
       return resolve(data);
     });
-  }).then(data => {
+  })
+    .then((data) => {
       noble.inbox["gyroscope"].x = data.readFloatLE(4);
       noble.inbox["gyroscope"].y = data.readFloatLE(8);
       noble.inbox["gyroscope"].z = data.readFloatLE(12);
       return;
-  }).catch(error => {
+    })
+    .catch((error) => {
       loggerChild.error(error);
       noble.inbox["gyroscope"].x = null;
       noble.inbox["gyroscope"].y = null;
       noble.inbox["gyroscope"].z = null;
       return;
-  })
-}
+    });
+};
 
 const getVoltage = () => {
   return new Promise((resolve, reject) => {
     noble.characteristics["voltage"].instance.read((error, data) => {
       if (error !== null) {
-          return reject(error);
+        return reject(error);
       }
       return resolve(data);
     });
-  }).then(data =>{
-    noble.inbox["voltage"].value = data.readFloatLE(4);
-      if (noble.inbox["voltage"].value <= 1.2 && noble.inbox["voltage"].value > 0) {
-          loggerChild.warn("battery voltage is low!! :" +  noble.inbox["voltage"].value + "V")
+  })
+    .then((data) => {
+      noble.inbox["voltage"].value = data.readFloatLE(4);
+      if (
+        noble.inbox["voltage"].value <= 1.2 &&
+        noble.inbox["voltage"].value > 0
+      ) {
+        loggerChild.warn(
+          "battery voltage is low!! :" + noble.inbox["voltage"].value + "V"
+        );
       }
       return;
-  }).catch(error => {
+    })
+    .catch((error) => {
       loggerChild.error(error);
       noble.inbox["voltage"].value = null;
       return;
-  })
-}
+    });
+};
 
 module.exports = noble;
--- routes/train.js
+++ routes/train.js
@@ -1,207 +1,223 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { STATE } = require('../constants');
-const iottrain = require('../iottrain_central');
-const { logger } = require('../logger.js');
-const loggerChild = logger.child({ domain: 'train' })
+const { STATE } = require("../constants");
+const iottrain = require("../iottrain_central");
+const { logger } = require("../logger.js");
+const loggerChild = logger.child({ domain: "train" });
 
-router.get('/', async(req, res, next) => {
-    try {
-        if ( req.app.get('state') === STATE.READY ||
-            req.app.get('state') === STATE.GOAL) {
-            
-            res.status(403).json(
-                {
-                    status: 'Forbidden',
-                    message: 'Request not currently allowed',
-                }
-            );
-            return;
-        }   
- 
-        // await Promise.all([
-        //     getAccelerometer(),
-        //     getGyroscope(),
-        //     getVoltage(),
-        // ]);
-
-        const accel = iottrain.inbox.accelerometer;
-        const gyro = iottrain.inbox.gyroscope;
-        const temp = iottrain.inbox.temperature;
-        const volt = iottrain.inbox.voltage;
-        
-        console.log("accel@" + accel.timestamp + ": " + accel.x + ", " + accel.y + ", " + accel.z);
-        console.log(" gyro@" + gyro.timestamp + ": " + gyro.x + ", " + gyro.y + ", " + gyro.z);
-        console.log(" temp@" + temp.timestamp + ": " + temp.value + "℃");
-        console.log(" volt@" + volt.timestamp + ": " + volt.value + "V");
-
-        const param = {
-            'accel': {
-                'x': accel.x, 
-                'y': accel.y, 
-                'z': accel.z, 
-            },
-            'gyro': {
-                'x': gyro.x, 
-                'y': gyro.y, 
-                'z': gyro.z, 
-            },
-            'volt': volt.value, 
-        };
-        res.header('Content-Type', 'application/json; charset=utf-8')
-        res.send(param);
-        return
-    } catch (error) {
-        loggerChild.error(error);
-        res.header('Content-Type', 'application/json; charset=utf-8')
-        res.status(500).json(
-            {
-                status: 'Internal Server Error'
-            }
-        );
-        return;
-    } finally {
-        loggerChild.info(req.method + ' ' + req.originalUrl + ' code: ' + res.statusCode);
-        return;
+router.get("/", async (req, res, next) => {
+  try {
+    if (
+      req.app.get("state") === STATE.READY ||
+      req.app.get("state") === STATE.GOAL
+    ) {
+      res.status(403).json({
+        status: "Forbidden",
+        message: "Request not currently allowed",
+      });
+      return;
     }
+
+    // await Promise.all([
+    //     getAccelerometer(),
+    //     getGyroscope(),
+    //     getVoltage(),
+    // ]);
+
+    const accel = iottrain.inbox.accelerometer;
+    const gyro = iottrain.inbox.gyroscope;
+    const temp = iottrain.inbox.temperature;
+    const volt = iottrain.inbox.voltage;
+
+    console.log(
+      "accel@" +
+        accel.timestamp +
+        ": " +
+        accel.x +
+        ", " +
+        accel.y +
+        ", " +
+        accel.z
+    );
+    console.log(
+      " gyro@" + gyro.timestamp + ": " + gyro.x + ", " + gyro.y + ", " + gyro.z
+    );
+    console.log(" temp@" + temp.timestamp + ": " + temp.value + "℃");
+    console.log(" volt@" + volt.timestamp + ": " + volt.value + "V");
+
+    const param = {
+      accel: {
+        x: accel.x,
+        y: accel.y,
+        z: accel.z,
+      },
+      gyro: {
+        x: gyro.x,
+        y: gyro.y,
+        z: gyro.z,
+      },
+      volt: volt.value,
+    };
+    res.header("Content-Type", "application/json; charset=utf-8");
+    res.send(param);
+    return;
+  } catch (error) {
+    loggerChild.error(error);
+    res.header("Content-Type", "application/json; charset=utf-8");
+    res.status(500).json({
+      status: "Internal Server Error",
+    });
+    return;
+  } finally {
+    loggerChild.info(
+      req.method + " " + req.originalUrl + " code: " + res.statusCode
+    );
+    return;
+  }
 });
 
-router.put('/', async(req, res, next) => {
-    try {
-        if (!req.app.get('allowOpReqToTrain')) {       
-            res.status(403).json(
-                {
-                    status: 'Forbidden',
-                    message: 'Request not currently allowed',
-                }
-            );
-            return;
-        }
-        const pwm = Number(req.query.pwm)
-        if (pwm === NaN || !(pwm >= 0 && pwm <= 100)) {
-            res.header('Content-Type', 'application/json; charset=utf-8')
-            res.status(400).json(
-                {
-                    status: 'Bad Request',
-                    message: 'pwm not specified or out of range',
-                }
-            );
-            return;
-        }
-
-        const err = await setPWM(pwm);
-
-        res.header('Content-Type', 'application/json; charset=utf-8')
-        if (err == null) {
-            res.status(200).json(
-                {
-                    status: 'OK',
-                }
-            );
-        } else {
-            loggerChild.error(err)
-            res.status(500).json(
-                {
-                    status: 'Internal Server Error',
-                }
-            );
-        }
-        return;
-    } catch (error) {
-        loggerChild.error(error);
-        res.header('Content-Type', 'application/json; charset=utf-8')
-        res.status(500).json(
-            {
-                status: 'Internal Server Error',
-            }
-        );
-        return; 
-    } finally {
-        loggerChild.info(req.method + ' ' + req.originalUrl + ' code: ' + res.statusCode);
-        return;
+router.put("/", async (req, res, next) => {
+  try {
+    if (!req.app.get("allowOpReqToTrain")) {
+      res.status(403).json({
+        status: "Forbidden",
+        message: "Request not currently allowed",
+      });
+      return;
     }
+    const pwm = Number(req.query.pwm);
+    if (pwm === NaN || !(pwm >= 0 && pwm <= 100)) {
+      res.header("Content-Type", "application/json; charset=utf-8");
+      res.status(400).json({
+        status: "Bad Request",
+        message: "pwm not specified or out of range",
+      });
+      return;
+    }
+
+    const err = await setPWM(pwm);
+
+    res.header("Content-Type", "application/json; charset=utf-8");
+    if (err == null) {
+      res.status(200).json({
+        status: "OK",
+      });
+    } else {
+      loggerChild.error(err);
+      res.status(500).json({
+        status: "Internal Server Error",
+      });
+    }
+    return;
+  } catch (error) {
+    loggerChild.error(error);
+    res.header("Content-Type", "application/json; charset=utf-8");
+    res.status(500).json({
+      status: "Internal Server Error",
+    });
+    return;
+  } finally {
+    loggerChild.info(
+      req.method + " " + req.originalUrl + " code: " + res.statusCode
+    );
+    return;
+  }
 });
 
 const getAccelerometer = () => {
-    return new Promise((resolve, reject) => {
-        iottrain.characteristics["accelerometer"].instance.read((error, data) => {
-            if (error !== null) {
-                return reject(error);
-            }    
-            return resolve(data);
-        })
-    }).then(data => {
-        iottrain.inbox["accelerometer"].x = data.readFloatLE(4);
-        iottrain.inbox["accelerometer"].y = data.readFloatLE(8);
-        iottrain.inbox["accelerometer"].z = data.readFloatLE(12);
-        return;
-    }).catch(error => {
-        loggerChild.error(error);
-        iottrain.inbox["accelerometer"].x = null;
-        iottrain.inbox["accelerometer"].y = null;
-        iottrain.inbox["accelerometer"].z = null;
-        return;
+  return new Promise((resolve, reject) => {
+    iottrain.characteristics["accelerometer"].instance.read((error, data) => {
+      if (error !== null) {
+        return reject(error);
+      }
+      return resolve(data);
+    });
+  })
+    .then((data) => {
+      iottrain.inbox["accelerometer"].x = data.readFloatLE(4);
+      iottrain.inbox["accelerometer"].y = data.readFloatLE(8);
+      iottrain.inbox["accelerometer"].z = data.readFloatLE(12);
+      return;
     })
-  }
-  
+    .catch((error) => {
+      loggerChild.error(error);
+      iottrain.inbox["accelerometer"].x = null;
+      iottrain.inbox["accelerometer"].y = null;
+      iottrain.inbox["accelerometer"].z = null;
+      return;
+    });
+};
+
 const getGyroscope = () => {
-    return new Promise((resolve, reject) => {
-        iottrain.characteristics["gyroscope"].instance.read((error, data) => {
-            if (error !== null) {
-                return reject(error);
-            }
-            return resolve(data);
-        });
-    }).then(data => {
-        iottrain.inbox["gyroscope"].x = data.readFloatLE(4);
-        iottrain.inbox["gyroscope"].y = data.readFloatLE(8);
-        iottrain.inbox["gyroscope"].z = data.readFloatLE(12);
-        return;
-    }).catch(error => {
-        loggerChild.error(error);
-        iottrain.inbox["gyroscope"].x = null;
-        iottrain.inbox["gyroscope"].y = null;
-        iottrain.inbox["gyroscope"].z = null;
-        return;
+  return new Promise((resolve, reject) => {
+    iottrain.characteristics["gyroscope"].instance.read((error, data) => {
+      if (error !== null) {
+        return reject(error);
+      }
+      return resolve(data);
+    });
+  })
+    .then((data) => {
+      iottrain.inbox["gyroscope"].x = data.readFloatLE(4);
+      iottrain.inbox["gyroscope"].y = data.readFloatLE(8);
+      iottrain.inbox["gyroscope"].z = data.readFloatLE(12);
+      return;
     })
-}
-  
+    .catch((error) => {
+      loggerChild.error(error);
+      iottrain.inbox["gyroscope"].x = null;
+      iottrain.inbox["gyroscope"].y = null;
+      iottrain.inbox["gyroscope"].z = null;
+      return;
+    });
+};
+
 const getVoltage = () => {
-    return new Promise((resolve, reject) => {
-        iottrain.characteristics["voltage"].instance.read((error, data) => {
-            if (error !== null) {
-                return reject(error);
-            }
-            return resolve(data);
-        });
-    }).then(data =>{
-        iottrain.inbox["voltage"].value = data.readFloatLE(4);
-        if (iottrain.inbox["voltage"].value <= 1.2) {
-            loggerChild.warn("battery voltage is low!! :" +  iottrain.inbox["voltage"].value + "V")
-        }
-        return;
-    }).catch(error => {
-        loggerChild.error(error);
-        iottrain.inbox["voltage"].value = null;
-        return;
+  return new Promise((resolve, reject) => {
+    iottrain.characteristics["voltage"].instance.read((error, data) => {
+      if (error !== null) {
+        return reject(error);
+      }
+      return resolve(data);
+    });
+  })
+    .then((data) => {
+      iottrain.inbox["voltage"].value = data.readFloatLE(4);
+      if (iottrain.inbox["voltage"].value <= 1.2) {
+        loggerChild.warn(
+          "battery voltage is low!! :" + iottrain.inbox["voltage"].value + "V"
+        );
+      }
+      return;
     })
-}
+    .catch((error) => {
+      loggerChild.error(error);
+      iottrain.inbox["voltage"].value = null;
+      return;
+    });
+};
 
 const setPWM = (pwm) => {
-    return new Promise((resolve, reject) => {
-        iottrain.characteristics["pwm"].instance.write(new Buffer.from([pwm]), false, (error) => {
-            if (error !== null) {
-                return reject(error);
-            }
-            return resolve();
-        });
-    }).then(() =>{
-        return;
-    }).catch(error => {
-        loggerChild.error(error);
-        iottrain.inbox["voltage"].value = null;
-        return error;
+  return new Promise((resolve, reject) => {
+    iottrain.characteristics["pwm"].instance.write(
+      new Buffer.from([pwm]),
+      false,
+      (error) => {
+        if (error !== null) {
+          return reject(error);
+        }
+        return resolve();
+      }
+    );
+  })
+    .then(() => {
+      return;
     })
-}
+    .catch((error) => {
+      loggerChild.error(error);
+      iottrain.inbox["voltage"].value = null;
+      return error;
+    });
+};
 
 module.exports = router;
