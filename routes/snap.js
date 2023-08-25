0df806b6d0075c6cdd667fa0b184de4561cd9f4b
Aguru Morita <morita584@yahoo.co.jp>
2023-08-26 08:01:55 +0900

    In the image upload API, changed to return Bad Request if id is other than 1 to 300.

--- routes/snap.js
+++ routes/snap.js
@@ -21,7 +21,7 @@ router.post(
         throw new RequestError(403, "Request not currently allowed");
       }
       const id = req.query.id;
-      if (id === "") {
+      if (id === undefined || !(id >= 1 && id <= 300)) {
         throw new RequestError(400, "Bad Request");
       }
       if (req.get("Content-Type") !== "image/png") {
